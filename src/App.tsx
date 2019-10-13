f597e06584e1655685ffb828d2e29a79a4021413
zhangyu <grewer@grewer.cn>
2019-10-13 23:54:00 +0800

    关于 computed

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,11 @@ class App extends React.Component<any> {
   render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
     const store = this.props.store;
     return <div>
-      app
-      <span>{store.firstState}</span>
+      <div>
+        app
+      </div>
+      <div>{store.firstState}</div>
+      <div>{store.firstStateChange}</div>
       <button onClick={() => store.onClick(123)}>change Value</button>
     </div>
   }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
-import { action, observable } from "mobx";
+import { action, computed, observable } from "mobx";
 
 class mainStore {
   @observable firstState = 0;
   @action onClick = (num) => {
     console.log('触发了action')
-    this.firstState+=num
+    this.firstState += num
+  }
+
+  @computed get firstStateChange() { // 不加computed 也可以  加了之后又类似缓存的性能提升
+    return this.firstState * 0.01
   }
 }
 
