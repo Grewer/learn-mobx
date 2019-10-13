7cb2f89305c76524e058f34927cef46c2750e39a
zhangyu <grewer@grewer.cn>
2019-10-13 22:59:14 +0800

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+## hooks 全局存储解决方案
+插件 react-hooks-redux-store
+
+//npm i @types/styled-components@4.1.8 --save-dev --save-exact
---
+++ src/ToDoList.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import {useModal} from "./reducer";
+
+function ToDoList() {
+  const [inp, setInp] = useState('')
+  const [state, dispatch] = useModal(['listReducer'])
+  console.warn('render todo List', state)
+  return <>
+    <input type="text" value={inp} onChange={ev => setInp(ev.target.value)}/>
+    <ul>
+      {
+        state.listReducer.map(v => <li key={v}>{v}</li>)
+      }
+    </ul>
+    <button onClick={() => dispatch({type: "ADD_LIST", payload: inp})}>add list</button>
+  </>
+}
+
+export default React.memo(ToDoList)
